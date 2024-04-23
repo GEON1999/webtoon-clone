ec86e91f5e2ac9ab487e3214e1e2592f546beeb2
geon <phgst@playdonut.com>
2024-04-23 11:14:20 +0900

    fetching api data

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/%08services/api_service.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeSCreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
