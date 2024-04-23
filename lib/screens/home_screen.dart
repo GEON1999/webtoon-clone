04a68912518bb0660891c14edaf923af37791619
geon <phgst@playdonut.com>
2024-04-23 11:17:52 +0900

    state 를 통한 데이터 활용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,143 +1,43 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:toonflix/%08services/api_service.dart';
+import 'package:toonflix/models/webtoon_model.dart';
 
-class HomeSCreen extends StatefulWidget {
-  const HomeSCreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomeSCreen> createState() => _HomeSCreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeSCreenState extends State<HomeSCreen> {
-  static const initailSec = 1500;
-  int totalSeconds = initailSec;
-  int totalPomodoros = 0;
-  bool isRunning = false;
-  late Timer timer;
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
 
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      timer.cancel();
-      setState(() {
-        totalPomodoros++;
-        isRunning = false;
-        totalSeconds = 1500;
-      });
-    } else {
-      setState(() {
-        totalSeconds--;
-      });
-    }
-  }
-
-  void onStopPressed() {
-    timer.cancel();
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
     setState(() {
-      totalSeconds = initailSec;
-      isRunning = false;
+      isLoading = false;
     });
   }
 
-  void onPressed() {
-    if (isRunning == true) {
-      setState(() {
-        isRunning = false;
-      });
-      timer.cancel();
-    } else {
-      timer = Timer.periodic(
-        const Duration(seconds: 1),
-        onTick,
-      );
-      setState(() {
-        isRunning = true;
-      });
-    }
-  }
-
-  String timeFormat() {
-    var duration = Duration(seconds: totalSeconds);
-    return '${duration.inMinutes.remainder(60).toString().padLeft(2, '0')}:${duration.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    waitForWebtoons();
   }
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(
-        children: [
-          Flexible(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(timeFormat(),
-                    style: TextStyle(
-                        color: Theme.of(context).cardColor,
-                        fontSize: 89,
-                        fontWeight: FontWeight.w600)),
-              )),
-          Flexible(
-              flex: 2,
-              child: Row(children: [
-                // center
-                IconButton(
-                    iconSize: 150,
-                    color: Theme.of(context).cardColor,
-                    onPressed: () {
-                      onPressed();
-                    },
-                    icon: Icon(isRunning
-                        ? Icons.pause_circle_outline
-                        : Icons.play_circle_outline)),
-                IconButton(
-                    iconSize: 150,
-                    color: Theme.of(context).cardColor,
-                    onPressed: () {
-                      onStopPressed();
-                    },
-                    icon: const Icon(Icons.stop_circle_outlined))
-              ])),
-          Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).cardColor,
-                          borderRadius: BorderRadius.circular(40)),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Pomodoros',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .displayLarge!
-                                    .color,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          Text(
-                            '$totalPomodoros',
-                            style: TextStyle(
-                                fontSize: 58,
-                                color: Theme.of(context)
-                                    .textTheme
-                                    .displayLarge!
-                                    .color,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-        ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 10,
+        backgroundColor: Colors.green,
+        title: const Text("웹툰",
+            style: TextStyle(color: Colors.white, fontSize: 20)),
       ),
     );
   }
