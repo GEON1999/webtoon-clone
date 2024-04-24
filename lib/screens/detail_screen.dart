e8276b9777ca5df2ca2439312e3ed3f47fe2189a
geon <phgst12@gmail.com>
2024-04-24 12:57:56 +0900

    animation with hero widget

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -21,25 +21,28 @@ class DetailScreen extends StatelessWidget {
             const SizedBox(
               height: 50,
             ),
-            Container(
-              height: 300,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12.withOpacity(0.8),
-                    offset: const Offset(0, 2),
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Image.network(
-                thumb,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
+            Hero(
+              tag: id,
+              child: Container(
+                height: 300,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12.withOpacity(0.8),
+                      offset: const Offset(0, 2),
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                  headers: const {
+                    "User-Agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  },
+                ),
               ),
             ),
             Text(
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -21,25 +21,28 @@ class Webtton extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Container(
-              height: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12.withOpacity(0.8),
-                    offset: const Offset(0, 2),
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              child: Image.network(
-                thumb,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
+            Hero(
+              tag: id,
+              child: Container(
+                height: 250,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12.withOpacity(0.8),
+                      offset: const Offset(0, 2),
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                  headers: const {
+                    "User-Agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  },
+                ),
               ),
             ),
             Text(
